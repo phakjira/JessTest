88f8de7c5b0c5326e43b2e4f50c28ab1eb652422
phakjira <phakjira2000@gmail.com>
2019-01-19 20:05:48 +0700

    added 2 number

--- Jess.java
+++ Jess.java
@@ -3,11 +3,7 @@ public class Jess{
   public static void main(String[] args){
     Scanner kb = new Scanner(System.in);
     System.out.println("Hi this is Jessie");
-<<<<<<< HEAD
-    System.out.println("Enter 2 number");
-=======
-    System.out.println("Enter one number");
->>>>>>> f5c21e5367c4259e1524b3755e8099bce8fe082d
+    System.out.println("Enter 2 numbers");
     int x = kb.nextInt();
 
   }
