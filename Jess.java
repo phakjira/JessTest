c2e66196f3034ba2fddf0fd7576dc621f2ae912a
phakjira <phakjira2000@gmail.com>
2019-01-19 19:37:34 +0700

    added a scanner

--- Jess.java
+++ Jess.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
 public class Jess{
   public static void main(String[] args){
+    Scanner kb = new Scanner(System.in);
     System.out.println("Hi this is Jessie");
+    System.out.println("Enter a number");
+    int x = kb.nextInt();
+
   }
 }
