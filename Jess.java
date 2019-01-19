799a0705b5438c15672fa60cc2c028a50b0f6a9d
phakjira <phakjira2000@gmail.com>
2019-01-19 19:54:08 +0700

    2 numbers

--- Jess.java
+++ Jess.java
@@ -3,7 +3,7 @@ public class Jess{
   public static void main(String[] args){
     Scanner kb = new Scanner(System.in);
     System.out.println("Hi this is Jessie");
-    System.out.println("Enter a number");
+    System.out.println("Enter 2 number");
     int x = kb.nextInt();
 
   }
