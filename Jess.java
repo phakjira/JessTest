f5c21e5367c4259e1524b3755e8099bce8fe082d
phakjira <phakjira2000@gmail.com>
2019-01-19 19:50:45 +0700

    one number

--- Jess.java
+++ Jess.java
@@ -3,7 +3,7 @@ public class Jess{
   public static void main(String[] args){
     Scanner kb = new Scanner(System.in);
     System.out.println("Hi this is Jessie");
-    System.out.println("Enter a number");
+    System.out.println("Enter one number");
     int x = kb.nextInt();
 
   }
